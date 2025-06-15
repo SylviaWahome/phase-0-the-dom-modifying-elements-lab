654492e0b648b873e672cedb31f07e35650bf2e0
Shee-001 <sylviawanjiru804@gmail.com>
2025-06-16 01:15:00 +0300

    Complete DOM nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Write your code here!// Remove the existing <main> element
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Assign it an id of "victory"
+newHeader.id = "victory";
+
+// Set its text content
+newHeader.textContent = "Paula Musuva is the champion";
+
+// Append it to the body
+document.body.append(newHeader);
